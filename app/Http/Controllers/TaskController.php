ae22e1340f39f0b03bda2125f8f1a67aca2ec94f
nongnapat2017 <n.ngnapat@gmail.com>
2017-10-27 00:55:00 -0400

    test

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Task;
+
+use Illuminate\Support\Facades\Input;
+
+use Carbon\Carbon;
+
+class TaskController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+
+
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create($project)
+    {
+        $tasks = \App\Task::all()->where('project',$project);
+        return view('todo-tasks',compact('tasks','project'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $task = new Task;
+        $task->project = Input::get('project');
+        $task->note = Input::get('task');
+        $task->status = 0;
+        $task->save();
+        return back();
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit()
+    {
+      
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request)
+    {
+        $task = Task::findOrFail($request->id);
+        $task->status = 1; 
+        $task->update();
+        return back();
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ resources/views/todo-tasks.blade.php
@@ -0,0 +1,73 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="robots" content="noindex, nofollow">
+
+    <title>Todo Example - Bootsnipp.com</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="//netdna.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <style>
+        html, body {
+            font-family: 'Lato', Arial;
+        }
+        body {
+            padding-top: 30px;
+        }
+        li.active {
+            color: #666663!important;
+            border-color: #E8E8E8!important;
+            background-color: #fffff6!important;
+            text-align: center;
+            font-weight: bold;
+            font-style: italic;
+        }
+    </style>
+    <title>TODO - List</title>
+</head>
+    <body>
+        <div class="container">
+            <div class="col-sm-12">
+                <form action="/todo/create" method="POST">
+                    <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                    <input type="hidden" name="project" value="{{ $project }}">
+                    <div class="alert alert-info" role="alert">
+                        <div class="text-center">
+                            <input type="text" name="task" class="form-control input-lg" placeholder="type your Task then press ENTER">
+                        </div>
+                    </div>
+                </form>
+            </div>
+
+            <div class="col-sm-6">
+                <ul class="list-group">
+                    <li class="list-group-item active">Todo Tasks</li>
+                        @foreach ($tasks as $task)
+                            @if($task->status ==0)
+                                <a href="/edit?_token={{ csrf_token() }}&id={{ $task->id }}" type="button" class="list-group-item list-group-item-warning">
+                                <span class="badge">{{ $task->created_at->diffForHumans() }}</span>
+                                {{ $task->note }}
+                                </a>
+                            @endif
+                        @endforeach 
+                </ul>
+            </div>
+
+            <div class="col-sm-6">
+                <ul class="list-group">
+                    <li class="list-group-item active">Completed Tasks</li>
+                        @foreach($tasks as $task)
+                            @if($task->status == 1)
+                                <li class="list-group-item list-group-item-success">
+                                <span class="badge">{{ $task->updated_at->diffForHumans() }}</span>
+                                <s>{{ $task->note }}</s>
+                                </li>
+                            @endif
+                        @endforeach
+                </ul>
+            </div>
+        </div>
+	</script>
+</body>
+</html>
